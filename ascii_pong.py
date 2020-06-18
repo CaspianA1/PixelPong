812c1c9c7412944241d4e297b62412962fae8cd5
CaspianA1 <caspianahlberg@gmail.com>
2020-06-18 12:34:33 -0400

    Fix paddle collision

--- ascii_pong.py
+++ ascii_pong.py
@@ -1,7 +1,7 @@
 # ascii_pong.py
 
 import curses, time
-from random import randint as ra
+from random import randint as ra, choice as ch
 
 
 
@@ -47,9 +47,10 @@ def main(screen):
 # cool colored background (maybe) - done
 # colored paddle - done
 # random starting ball position - done
-# hit the paddle
+# hit the paddle - done
 # make the paddle faster than the ball, or give it some acceleration - done
 # add a second paddle - done
+# increase baud rate of terminal - not sure how to do?
 
 left_paddle_y = 0
 right_paddle_y = 0
@@ -70,21 +71,12 @@ def move_ball(screen):
 	wall_bounce(screen)
 
 	if b_going_right: ball_pos[0] += 1
-	else: ball_pos[0] -= 2
+	else: ball_pos[0] -= 1
 
 	if b_going_down: ball_pos[1] += 1
-	else: ball_pos[1] -= 2
+	else: ball_pos[1] -= 1
 
 	x, y = screen.getmaxyx()[::-1]
-
-	if ball_pos[0] < 0:
-		ball_pos[0] = 0
-	elif ball_pos[0] > x:
-		ball_pos[0] = x
-	if ball_pos[1] < 0:
-		ball_pos[1] = 0
-	elif ball_pos[1] > y:
-		ball_pos[1] = y
 
 	put_char(screen, *ball_pos, "*")
 
@@ -103,44 +95,30 @@ def wall_bounce(screen):
 		left_score += 1
 	if ball_pos[1] == 0:
 		b_going_down = True
-	elif ball_pos[1] == y - 1:
+	elif ball_pos[1] == y:
 		b_going_down = False
 
 def hit_paddle(screen):
 	global b_going_right, b_going_down
-	"""
-	if ball_pos[0] in (
-		range(PADDLE_DIST_WALL - 1, PADDLE_DIST_WALL + 1),
-		range(screen.getmaxyx()[1] - 1, screen.getmaxyx()[1] + 1)):
-		# screen.getmaxyx()[1] - PADDLE_DIST_WALL + 1,
-		# screen.getmaxyx()[1] - PADDLE_DIST_WALL - 1):
-	"""
 
-
-	candidate_x_positions = [
-	PADDLE_DIST_WALL - 2,
-	PADDLE_DIST_WALL + 2,
-	screen.getmaxyx()[1] - PADDLE_DIST_WALL - 2,
-	screen.getmaxyx()[1] - PADDLE_DIST_WALL + 2
-	]
-
-	# fix collision error
-	if ball_pos[0] in candidate_x_positions:
-		if ball_pos[1] in range(left_paddle_y - 1, left_paddle_y + PADDLE_HEIGHT):
-			screen.addstr(15, 15, "Condition one")
+	# left paddle x collision
+	if ball_pos[0] in (PADDLE_HEIGHT - 1, PADDLE_HEIGHT + 1):
+		# left paddle y collision
+		if ball_pos[1] in range(left_paddle_y, left_paddle_y + PADDLE_HEIGHT + 1):
+			curses.flash()
 			b_going_right = not b_going_right
-			# b_going_down = not b_going_down
-		elif ball_pos[1] in range(right_paddle_y - 1, right_paddle_y + PADDLE_HEIGHT):
-			screen.addstr(15, 15, "Condition two")
+
+	# right paddle x collision
+	elif ball_pos[0] in (screen.getmaxyx()[1] - PADDLE_DIST_WALL - 1,
+		screen.getmaxyx()[1] - PADDLE_DIST_WALL + 1):
+		# right paddle y position
+		if ball_pos[1] in range(right_paddle_y, right_paddle_y + PADDLE_HEIGHT - 1):
+			curses.flash()
+			curses.beep()
 			b_going_right = not b_going_right
-			# b_going_down = not b_going_down
-		else:
-			screen.addstr(15, 15, "Condition three")
-	else:
-		screen.addstr(15, 15, "Condition four")
 
 char_ascii = lambda screen, char: screen.getch() == ord(char)
-char_special = lambda screen, spec: screen.getch() == spec
+# char_special = lambda screen, spec: screen.getch() == spec
 
 def put_char(screen, x, y, s, color = None):
 	try:
@@ -154,6 +132,7 @@ def put_char(screen, x, y, s, color = None):
 
 def main(screen):
 	global left_paddle_y, right_paddle_y, ball_pos
+	global b_going_right, b_going_down
 
 	rand_x = lambda: ra(0, screen.getmaxyx()[1])
 	rand_y = lambda: ra(0, screen.getmaxyx()[0] - PADDLE_HEIGHT - 1)
@@ -164,39 +143,65 @@ def main(screen):
 	left_paddle_y = rand_y()
 	right_paddle_y = rand_y()
 
-	curses.noecho()
-	screen.keypad(True)
+	b_going_right = ch((True, False))
+	b_going_down = ch((True, False))
+
+	# curses.noecho()
+	# curses.cbreak()
+	# screen.keypad(True)  # .wrapper initializes these by default
+	# curses.noraw()
+	curses.raw()
+	curses.savetty()
+
 	screen.nodelay(True)
 
 	curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_CYAN)  # screen
 	screen.bkgd(" ", curses.color_pair(1))
 
-	curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_BLACK)  # left paddle
+	curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_BLACK)  # left and right paddle
 
 	while not char_ascii(screen, "q"):
-		y, x = screen.getmaxyx()
+		_, x = screen.getmaxyx()
+		#screen.addstr(0, 0, f"Ball x and y: {ball_pos[0]} {ball_pos[1]}")
+		#sscreen.addstr(1, 0, f"Left paddle y: {left_paddle_y}")
 		screen.addstr(0, x // 3, f"| Player 1 Score: {left_score}   Player 2 score: {right_score}  |")
 		screen.addstr(1, x // 3, r"|_\/_\/_\/_\/_\/_\/_\/_\/_\/_\/_\/_\/_\/\|")
 
-		# if char_special(screen, curses.KEY_UP) and paddle_y != 0:
+		max_y, max_x = screen.getmaxyx()
+
+		# maybe use ungetch somewhere here
+
 		if char_ascii(screen, "w") and left_paddle_y != 0:
 			left_paddle_y -= 2
 
-		elif char_ascii(screen, "s"):
-			if left_paddle_y != (screen.getmaxyx()[0] - PADDLE_HEIGHT - 1):
-				left_paddle_y += 2
+			if left_paddle_y < 0:
+				left_paddle_y = 0
 
-		if char_special(screen, curses.KEY_UP) and right_paddle_y != 0:
+		elif char_ascii(screen, "s"):
+			left_paddle_y += 2
+
+			if left_paddle_y + PADDLE_HEIGHT > max_y:
+				left_paddle_y = max_y - PADDLE_HEIGHT - 1
+
+		# if char_special(screen, curses.KEY_UP) and right_paddle_y != 0:
+		if char_ascii(screen, "i") and right_paddle_y != 0:
 			right_paddle_y -= 2
 
-		elif char_special(screen, curses.KEY_DOWN):
+			if right_paddle_y < 0:
+				right_paddle_y = 0
+
+		# elif char_special(screen, curses.KEY_DOWN):
+		elif char_ascii(screen, "k"):
 			if right_paddle_y != (screen.getmaxyx()[0] - PADDLE_HEIGHT - 1):
 				right_paddle_y += 2
 
-		# time.sleep(0.1)
-		time.sleep(0.185)
+			if right_paddle_y + PADDLE_HEIGHT > max_y:
+				right_paddle_y = max_y - PADDLE_HEIGHT - 1
 
+		# curses.napms(185)
 		screen.clear()
+		curses.napms(40)
+
 		for h in range(PADDLE_HEIGHT + 1):
 
 			put_char(screen,
@@ -211,7 +216,6 @@ def main(screen):
 			curses.color_pair(2))  # right paddle
 
 		move_ball(screen)
-
 		screen.refresh()
 
 
